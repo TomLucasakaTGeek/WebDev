96689a56f9950dd2fa6168ebf44556ea7ced477f
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2023-12-24 23:05:09 +0530

    control flow

---
+++ Javascript/101.js
@@ -0,0 +1,8 @@
+let myName="Tanmay"
+let age=20
+let isMarried=false
+console.log("This person is "+myName+" their age is "+age)
+if(isMarried == false)
+    console.log(myName+" is not married")
+else
+    console.log(myName+" is married")
