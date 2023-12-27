b4dd4c766c9c68a6e4e57aff9862c54ad8a70df3
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2023-12-27 16:11:49 +0530

    Promises

--- Javascript/103.js
+++ Javascript/103.js
@@ -1,23 +1,37 @@
 //Async 
-
-// function findSum(n){
-//     let ans=0
-//     for(let i=0;i<n;i++)
-//         ans+=i
-//     return ans
-// }
-// function findSumTill100(){
-//     console.log(findSum(100));
-// }
-// setTimeout(findSumTill100, 1000)
-// console.log("hello world");
-
-// const fs = require("fs")
+// const fs = require("fs");
 // fs.readFile("a.txt", "utf-8", function(err, data){
-//     console.log(data);
-// })
+//   console.log(data)
+// } )
+// console.log("Hi")
+// let a=0
+// for(let i=0; i<1000000000; i++) a++;
+// console.log("Hi there")
   
-//Promise
+//Promises
+// const fs = require("fs");
+
+// function Reedfile() {
+//   console.log("Inside Reedfile")
+//   return new Promise(function(resolve) {
+//     console.log("Inside Promise")
+//     fs.readFile("a.txt", "utf-8", function(err, data) {
+//       console.log("Before Resolve")
+//       resolve(data)
+//     } )
+//   } )
+// }
+
+// function onDone(data){
+//   console.log("Inside onDone")
+//   console.log(data)
+// }
+
+// console.log("Hi")
+// let value = Reedfile()
+// console.log(value)
+// value.then(onDone)
+// console.log("Hello2")
 
 
 
