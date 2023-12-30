bd9812efebe168aa72f8a0c6dfc799835cb78470
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2023-12-30 20:23:52 +0530

    React component

--- React-Projects/Project(W4-1)/src/App.jsx
+++ React-Projects/Project(W4-1)/src/App.jsx
@@ -3,20 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+let state = {
+  count:0
+}
+
 function App() {
-  const [count, setCount] = React.useState(0)
+  const [count, setCount]=useState(0)
+  function onClickHandler() {
+    state.count=state.count+1;
+    console.log(state.count)
+  }
 
   return (
     <div> 
-        <Button count={count} setCount={setCount}></Button>
+        <button onClick={onClickHandler}>Counter {state.count}</button>
     </div>
   )
 }
 
-function Button(props) {
-  function onButtonClick() {
-    props.setCount(props.count+1);
-  }
-  return <button onClick={onButtonClick}>Counter {props.count}</button>
-}
+// function Button(props) {
+//   function onButtonClick() {
+//     props.setCount(props.count+1);
+//   }
+//   return <button onClick={onButtonClick}>Counter {props.count}</button>
+// }
 export default App
