5ba895268dcf6f6e48386ea733ade0781590d57d
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2023-12-30 20:13:34 +0530

    React counting

--- React-Projects/Project(W4-1)/src/App.jsx
+++ React-Projects/Project(W4-1)/src/App.jsx
@@ -4,13 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = React.useState(0)
 
   return (
     <div> 
-        hi there
+        <Button count={count} setCount={setCount}></Button>
     </div>
   )
 }
 
+function Button(props) {
+  function onButtonClick() {
+    props.setCount(props.count+1);
+  }
+  return <button onClick={onButtonClick}>Counter {props.count}</button>
+}
 export default App
